c8d9ffc67688e5c7b11a0ff2d926cae51bae4b31
dLqovop <126761271+dLqovop@users.noreply.github.com>
2023-05-28 11:03:38 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/ui/notifications/trashdetective/Result.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.ui.notifications.trashdetective;
+
+import android.graphics.Rect;
+
+public class Result {
+    public int classIndex;
+    Float score;
+    Rect rect;
+
+    public Result(int cls, Float output, Rect rect) {
+        this.classIndex = cls;
+        this.score = output;
+        this.rect = rect;
+    }
+};
